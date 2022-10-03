7b51fd4eddd4897a3f1a1322fd055352fa329951
DenisChum <105285389+DenisChum@users.noreply.github.com>
2022-10-03 15:12:19 +0200

    add style

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,82 +1,75 @@
-import React, {Component} from "react";
-
-import FormAddContact from './FormAddContact/FormAddContact';
-// import ContactList from './ContactList/ContactList';
-// import Filter from './Filter/Filter';
-
+import React, { Component, Fragment } from 'react';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { nanoid } from 'nanoid';
 
+import FormAddContact from './FormAddContact/FormAddContact';
+import Filter from './Filter/Filter';
+import ContactList from './ContactList/ContactList';
+
+import { Container, Title, SecondTitle } from './App.styled';
+
 class App extends Component {
-  state = {
+state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
-  };
-
-  onAddContact = ({ name, number }) => {
+};
+onAddContact = ({ name, number }) => {
     const normalizedName = name.toLowerCase();
-
     if (
-      this.state.contacts.some(
+    this.state.contacts.some(
         contact => contact.name.toLowerCase() === normalizedName
-      )
+    )
     ) {
-      Notify.failure(`${name} is already in contacts`);
-      return;
+    Notify.failure(`${name} is exist already in contacts`);
+    return;
     }
-
     this.setState(prevState => ({
-      contacts: [
+    contacts: [
         ...prevState.contacts,
         {
-          id: nanoid(),
-          name,
-          number,
+        id: nanoid(),
+        name,
+        number,
         },
-      ],
+    ],
     }));
-  };
-
-  handleDelete = id => {
+};
+    
+handleDelete = id => {
     this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    contacts: prevState.contacts.filter(contact => contact.id !== id),
     }));
-  };
-
-  handleFilter = e => {
+};
+handleFilter = e => {
     this.setState({ filter: e.target.value });
-  };
+};
 
-  render() {
-    const { contacts, filter } = this.state;
-    let renderContacts = contacts;
+render() {
+    const { contacts, filter } = this.state;    
 
-    renderContacts = contacts.filter(({ name }) =>
-      name.toLowerCase().includes(filter.trim())
+    let renderContacts = contacts.filter(({ name }) =>
+    name.toLowerCase().includes(filter.trim())
     );
-
+    
     return (
-      <div>
-        <h1>Phonebook</h1>
-        <div>
-          <FormAddContact onSubmit={this.onAddContact} />
-        </div>
-        <div>
-          <h2>Contacts</h2>
-          {/* <Filter handleFilter={this.handleFilter} value={filter} />
-          <ContactList
-            contacts={renderContacts}
-            handleDelete={this.handleDelete}
-          /> */}
-        </div>
-      </div>
+        <Fragment>
+            <Container>
+                <Title>Phonebook</Title>                
+                    <FormAddContact onSubmit={this.onAddContact} />                
+                <SecondTitle>Contacts</SecondTitle>                                   
+                    <Filter handleFilter={this.handleFilter}
+                    value={filter} />
+                    <ContactList contacts={renderContacts}
+                    handleDelete={this.handleDelete}/>                
+            </Container>
+        </Fragment>
     );
-  }
+}
 }
 
 export default App;
--- src/components/FormAddContact/FormAddContact.jsx
+++ src/components/FormAddContact/FormAddContact.jsx
@@ -1,64 +1,67 @@
 import React, { Component } from 'react';
-// import s from './ContactForm.module.css';
+import {
+    Form,
+    Label,
+    Input,
+    Button
+} from './FormAddContact.styled';
+
 
 export default class FormAddContact extends Component {
-  state = {
+state = {
     name: '',
     number: '',
-  };
+};
 
-  handleSubmit = e => {
+handleSubmit = e => {
     e.preventDefault();
-
+    
     const { onSubmit } = this.props;
     onSubmit(this.state);
     this.resetForm();
-  };
+};
 
-  resetForm = () => {
+resetForm = () => {
     this.setState(() => ({
-      name: '',
-      number: '',
+    name: '',
+    number: '',
     }));
-  };
-
-  handleChange = e => {
+};
+    handleChange = e => {    
     const { name, value } = e.currentTarget;
     this.setState({ [name]: value });
-  };
+};
 
-  render() {
+render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Name
-          <input
+    <Form onSubmit={this.handleSubmit}>
+        <Label>
+            Name
+        <Input
             type="text"
             name="name"
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             value={this.state.name}
             onChange={this.handleChange}
-            required
-          />
-        </label>
+            required />
+        </Label>
 
-        <label>
-          Number
-          <input
+        <Label>
+            Number
+        <Input
             type="tel"
             name="number"
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             value={this.state.number}
             onChange={this.handleChange}
-            required
-          />
-        </label>
-        <button type="submit">
-          Add contact
-        </button>
-      </form>
+            required />
+        </Label>
+        <Button type="submit">
+            Add contact
+        </Button>
+    </Form>
     );
-  }
+}
 }
